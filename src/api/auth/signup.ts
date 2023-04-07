cf9c425363e6c517eafafefcd3e7bbd761a32942
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-07 15:15:41 -0600

    Added bcrypt password hashing

--- src/api/auth/signup.ts
+++ src/api/auth/signup.ts
@@ -1,3 +1,4 @@
+import { hashPassword } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
 
@@ -5,8 +6,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, password } = req.body;
 
   try {
+    const hashedPassword = await hashPassword(password);
+
     const user = await prisma.user.create({
-      data: { email, password },
+      data: { email, password: hashedPassword },
     });
     res.status(201).json(user);
   } catch (error: any) {
