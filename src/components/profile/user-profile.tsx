3f9245a78d2d6188e839e35d36b05ba3a1254ea4
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-08 13:33:01 -0600

    Added redirection to /auth on logout

--- src/components/profile/user-profile.tsx
+++ src/components/profile/user-profile.tsx
@@ -1,11 +1,17 @@
 import { useSession } from 'next-auth/react';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
+import { useRouter } from 'next/router';
 
 function UserProfile() {
   const { data: session, status } = useSession();
+  const router = useRouter();
 
-  if (status === 'loading') {
+  if (status !== 'authenticated') {
+    if (status === 'unauthenticated') {
+      router.push('/auth');
+    }
+
     return <p className={classes.profile}>Loading...</p>;
   }
 
