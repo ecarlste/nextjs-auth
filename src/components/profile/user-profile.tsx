28c6f3e11793ddf3c744fc20961ddf439c98ec3c
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-10 13:48:13 -0600

    Switched profile check to server side props

--- src/components/profile/user-profile.tsx
+++ src/components/profile/user-profile.tsx
@@ -1,20 +1,7 @@
-import { useSession } from 'next-auth/react';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
-import { useRouter } from 'next/router';
 
 function UserProfile() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  if (status !== 'authenticated') {
-    if (status === 'unauthenticated') {
-      router.push('/auth');
-    }
-
-    return <p className={classes.profile}>Loading...</p>;
-  }
-
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,23 @@
+import { getSession } from 'next-auth/react';
 import UserProfile from '../components/profile/user-profile';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 
 function ProfilePage() {
   return <UserProfile />;
+}
+
+// const getServerSideProps: GetServerSideProps = async (context) => {};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: { destination: '/auth', permanent: false },
+    };
+  }
+
+  return { props: { session } };
 }
 
 export default ProfilePage;
